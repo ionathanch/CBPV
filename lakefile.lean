9fbc299a5862802720c7efd47d0a30fa2ca5479c
Jonathan Chan <jonathanhwchan@gmail.com>
2025-10-15 16:42:42 -0400

    Update mutual induction tactic version

--- lakefile.lean
+++ lakefile.lean
@@ -2,7 +2,7 @@ import Lake
 
 open Lake DSL
 
-require "ionathanch" / "MutualInduction" @ git "main"
+require "ionathanch" / "MutualInduction" @ git "v0.1.0"
   from git "https://github.com/ionathanch/MutualInduction" @ "main"
 
 package «CBPV» where
